4e58f9401f6d3ecee69e6d2d5c866cd767e17fef
Bohdan <b.chernov.kiev@gmail.com>
2020-11-01 19:09:47 +0200

    change2 from master

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
 
         System.out.println("change1");
+        System.out.println("change2 from master");
     }
 
 }
